2e5dcd485c5f33079f03f6d0f657fed1cc821568
Scarlett Eller <rq6715@gmail.com>
2022-01-14 13:50:34 -0500

    commit tree b11a2e03259990be8d6ccc30877803cf57a35b5a

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const newHeader = document.createElement('h1');
-document.body.appendChild(newHeader);
-newHeader.id = 'victory';
-victory.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
-
 main.remove();
+
+
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Scarlett is the champion'
+
 
